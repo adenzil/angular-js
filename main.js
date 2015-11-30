7a5556370f9830f2754a7b5c7b8776a54df87e92
Alister <alistercabral@gmail.com>
2015-11-30 15:43:07 +0530

    toggle name and count of button click

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
+    $scope.count = 0;
+    $scope.myVar = false;
+    $scope.toggle = function() {
+        $scope.myVar = !$scope.myVar;
+        $scope.buttonText = "SHOW";
+    };
+
+
 });
